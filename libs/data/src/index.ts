0d37ce997c82b5c376501dc3d3d7d693036481df
Behzad Barzin <behzad_barzin@live.com>
2025-08-12 02:40:01 +0330

    Add auto type generation from OpenAPI docs

--- libs/data/src/index.ts
+++ libs/data/src/index.ts
@@ -1,2 +1,8 @@
 export * from "./types/jwt-payload.type.js";
 export * from "./types/request-user.type.js";
+
+export * as apiTypes from "./typed-api/api.types.js";
+export * from "./typed-api/api.client.js";
+
+export * as authTypes from "./typed-api/auth.types.js";
+export * from "./typed-api/auth.client.js";
